daf461726969b5c302ef087ca5ab36f5c9149d4e
Bryan Kveton <bkveton2@gmail.com>
2014-12-03 17:50:31 -0600

    Update to 'away message' website

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+relative_assets = true
+
+# output_style = :compressed
+# line_comments = false
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
-$(document).ready(function() {
-
-  $( ".show").click(function() {
-    $(this).toggleClass("active");
-    $(this).find(".show-details").toggleClass("dim");
-    $(this).find(".show-links").toggleClass("reveal");
+(function () {
+  $("required").click(function(){
+    $(this).attr("value","");
+    $(this).addClass("yo");
   });
-
-});
-
-$(window).load(function() {
-
-  $( "#bg-img, #page" ).removeClass("hide");
-
-});
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 750);
-        return false;
-      }
-    }
-  });
-});
+})();
